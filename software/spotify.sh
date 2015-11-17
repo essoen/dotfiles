cce1eb8fa8f0c0d3213a6c2d2e35ae0a584c3f4c
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-11-17 11:04:28 +0100

    Add spotify-client-gnome-support and libgcrypt11
    `libgcrypt11` is not available in official repos

--- software/spotify.sh
+++ software/spotify.sh
@@ -1,5 +1,11 @@
 # Install Spotify
+# You need libgcrypt11
+wget http://security.debian.org/debian-security/pool/updates/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_amd64.deb
+dpkg -i libgcrypt11_1.5.0-5+deb7u3_amd64.deb
+rm "libgcrypt11_1.5.0-5+deb7u3_amd64.deb"
+
+# And Spotify
 add-apt-repository "deb http://repository.spotify.com stable non-free"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 apt-get update
-apt-get install -y spotify-client
+apt-get install -y spotify-client spotify-client-gnome-support # Gnome support included as I use Gnome
