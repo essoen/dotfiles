fd50e160453e90dbc8f51d08ed6ca6f1578eb1cb
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-02-13 21:31:41 +0100

    Add Docker

--- software/apps-devtools.sh
+++ software/apps-devtools.sh
@@ -5,4 +5,6 @@ apt-get install -y gnome terminator curl git vim nano ftp ssh xlip tar
 apt-get install -y python python3 python-pip -python3-pip #Python and Pip
 apt-get install -y openjdk-7-jre openjdk-7-jdk  openjdk-8-jre openjdk-8-jdk  # Java
 
+curl -fsSL https://get.docker.com/ | sh #Docker
+
 
