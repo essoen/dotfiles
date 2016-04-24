b72af4f741770e2203ddb9bfb0308354235cf530
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-04-24 14:41:20 +0200

    Update mediasoftware.sh

--- software/mediasoftware.sh
+++ software/mediasoftware.sh
@@ -2,17 +2,17 @@
 
 # Plex server
 wget "https://downloads.plex.tv/plex-media-server/0.9.9.14.531-7eef8c6/plexmediaserver_0.9.9.14.531-7eef8c6_amd64.deb"
-dbkg -i plexmediaserver*.deb
+sudo dbkg -i plexmediaserver*.deb
 
 # SickBeard
-apt-get install python-cheetah
+sudo apt-get install python-cheetah
 cd
 git clone git://github.com/midgetspy/Sick-Beard.git .sickbeard
 cd .sickbeard
 cp -a autoProcessTV/autoProcessTV.cfg.sample autoProcessTV/autoProcessTV.cfg
 
 # Sabnzbd
-add-apt-repository ppa:jcfp/ppa
-apt-get update
-apt-get install sabnzbdplus
+sudo add-apt-repository ppa:jcfp/ppa
+sudo apt-get update
+sudo apt-get install sabnzbdplus
 echo "Start with command sabnzbdplus \n" 
