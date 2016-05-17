058194936c82cca06b2dda1773c936a9e7780c18
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-05-17 19:15:14 +0200

    Bump Idea-version

--- software/idea-software.sh
+++ software/idea-software.sh
@@ -1,5 +1,5 @@
-# Install IntelliJ
-PACKAGE="ideaIU-2016.1.1.tar.gz"
+ Install IntelliJ
+PACKAGE="ideaIU-2016.1.2b.tar.gz"
 wget "https://download.jetbrains.com/idea/$PACKAGE"
 tar -xzf $PACKAGE
 sudo mv /opt
