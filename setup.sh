fa0454b0417580d6581550d0fee2a32c446ee212
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-10 11:31:29 +0200

    Add Atom configuration and setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm $HOME/.bashrc
 ln -s $PWD/bash/profile $HOME/.bashrc
 echo "Created symbolic link for .bash_profile"
@@ -13,3 +14,14 @@ echo "Created symbolic link for .gitconfig"
 rm $HOME/.ssh/config
 ln -s $PWD/ssh/config $HOME/.ssh/config
 echo "Created symbolic link for .ssh/config"
+
+echo "Creating symbolic links for Atom configuration files"
+ATOM_PATH="$HOME/.atom"
+ATOM_SETTINGS_PATH="$PWD/atom"
+
+for filepath in $ATOM_SETTINGS_PATH/*
+do
+    filename=${filepath##*/}
+    rm $ATOM_PATH/$filename
+    ln -s $ATOM_SETTINGS_PATH/$filename $ATOM_PATH/$filename
+done
