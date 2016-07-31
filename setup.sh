1c1e20511dbb015703d5e30a04b54cb00af81c0b
Stein-Otto Svorstøl <steinotto@svorstol.com>
2016-07-31 12:53:40 +0200

    Add vimrc

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ rm $HOME/.bash_aliases
 ln -s $PWD/bash/aliases $HOME/.bash_aliases
 echo "Created symbolic link for .bash_aliases"
 
+rm $HOME/.vimrc
+ln -s $PWD/bash/vimrc $HOME/.vimrc
+echo "Created symbolic link for .vimrc"
+
 rm $HOME/.gitconfig
 ln -s $PWD/git/gitconfig $HOME/.gitconfig
 echo "Created symbolic link for .gitconfig"
