0ff8364479edb8dbdaf37d5e10a253e26fa7ecb9
Stein-Otto Svorstøl <steinotto@svorstol.com>
2016-08-03 10:15:19 +0200

    Install xsel

--- software/setup.sh
+++ software/setup.sh
@@ -5,15 +5,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/software"
 echo "Starting installation"
 echo "Installing tools like git, curl,  vim, nano, ftp, ssh xclip and tar."
 # Install applications
-sudo apt-get install -y terminator curl git vim nano ftp ssh xclip tar
+sudo apt-get install -y terminator curl git vim nano ftp ssh xclip xsel tar
 echo "Installing and activating firewall"
 sudo apt-get install ufw
 sudo ufw enable 
 
 echo "Installing devtools"
-echo "First we install Python 2 and 3, along with pip. Then OpenJDK 7 and 8 + Vagrant"
+echo "First we install Python 2 and 3, along with pip, as well as virtualenv" 
 # Dev
-sudo apt-get install -y python python3 python-pip -python3-pip #Python and Pip
+sudo apt-get install -y python python3 python-pip python3-pip #Python and Pip
+# See http://askubuntu.com/questions/244641/how-to-set-up-and-use-a-virtual-python-environment-in-ubuntu
+pip3 install --user virtualenvwrapper
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+export WORKON_HOME=~/.virtualenvs
+mkdir $WORKON_HOME
+echo "export WORKON_HOME=$WORKON_HOME" >> ~/.bashrc
+echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+echo "export PIP_VIRTUALENV_BASE=$WORKON_HOME" >> ~/.bashrc 
+source ~/.bashrc
+
+echo "Install OpenJDK 7 and 8 + Vagrant"
 sudo apt-get install -y openjdk-7-jre openjdk-7-jdk  openjdk-8-jre openjdk-8-jdk  # Java
 sudo apt-get install -y vagrant
 
