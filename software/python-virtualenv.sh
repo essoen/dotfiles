49b0b8b7c537a1781fb7d432b82c45ce5e0d56ae
Stein-Otto Svorstøl <steinotto@svorstol.com>
2016-08-18 14:29:14 +0200

    Add script for python-virtualenv

---
+++ software/python-virtualenv.sh
@@ -0,0 +1,12 @@
+
+sudo apt-get install -y python python3 python-pip python3-pip #Python and Pip
+# See http://askubuntu.com/questions/244641/how-to-set-up-and-use-a-virtual-python-environment-in-ubuntu
+pip3 install --user virtualenvwrapper
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+export WORKON_HOME=~/.virtualenvs
+mkdir $WORKON_HOME
+echo "export WORKON_HOME=$WORKON_HOME" >> ~/.bashrc
+echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+echo "export PIP_VIRTUALENV_BASE=$WORKON_HOME" >> ~/.bashrc 
+source ~/.bashrc
+
