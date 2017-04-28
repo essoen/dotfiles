e23b506bc41318d23e1b889356c7e4324e6676e8
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2017-04-28 21:45:49 +0200

    Add Yarn

--- software/setup.sh
+++ software/setup.sh
@@ -49,6 +49,12 @@ source ~/.bashrc
 # Node
 nvm install latest
 
+echo "Installing yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
+
 echo "Installing Meteor"
 # Meteor
 curl https://install.meteor.com/ | sh
