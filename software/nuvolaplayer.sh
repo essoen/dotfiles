fa85a8a55680ae1f6790b38574221a4e5afa7015
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2025-04-21 15:29:27 +0200

    remove dead sw

--- software/nuvolaplayer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-codename=`lsb_release --codename | cut -f2`
-
-sudo add-apt-repository "deb https://tiliado.eu/nuvolaplayer/repository/deb/ $codename stable"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 40554B8FA5FE6F6A
-sudo apt-get update
-sudo apt-get install -y nuvolaplayer3
